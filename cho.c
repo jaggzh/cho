abfa3c55ef28d6fd0e0661f8956a4f759ee05f02
Jaggzh <jaggz.h@gmail.com>
2025-02-17 17:58:35 -0800

    Formatted nicer

--- cho.c
+++ cho.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-int main(int c, char *a[]) { if (c>1) fputs(a[1], stdout); for (int i=2; i<c; i++) { putchar(' '); fputs(a[i], stdout); } putchar('\n'); }
+
+int main(int c, char *a[]) {
+	if (c>1) fputs(a[1], stdout);
+	for (int i=2; i<c; i++) {
+		putchar(' ');
+		fputs(a[i], stdout);
+	}
+	putchar('\n');
+}
